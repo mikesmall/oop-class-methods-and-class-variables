a28f22b9b2471a89d23873eea9dffee6943df838
mikesmall <michael.l.a.small@gmail.com>
2017-05-23 16:57:45 -0400

    A few sub-steps through Part 2, step 4

--- part_2_book_lending.rb
+++ part_2_book_lending.rb
@@ -6,8 +6,31 @@ attr_accessor :due_date
 
 @@on_loan = [] # the collection of books that are currently being borrowed
 
+# initialize a book's title, author, and isbn.
+def initialize(title, author, isbn)
+  @title = title
+  @author = author
+  @isbn = isbn
+end#initialize
 
+# how new books are added to the library.
+def self.create
+  # call Book.new(...),
+  new_book = Book.new(title, author, isbn)
+  # add the new book object to @@on_shelf,
+  @@on_shelf << new_book
+  # and then return the new book
+  return new_book
+end#self.create
 
+# This class method should return the value of @@on_shelf.
+def self.available
+  @@on_shelf
+end#self.available
 
+# This class method should return the value of @@on_loan.
+def self.borrowed
+  @@on_loan
+end#self.borrowed  
 
 end#Book
