19edf2e691358d1c63b5351d7391ff3ad69802c9
mikesmall <michael.l.a.small@gmail.com>
2017-05-23 16:47:58 -0400

    Steps 1-3 of Part 2 (Book Lending) complete

---
+++ part_2_book_lending.rb
@@ -0,0 +1,13 @@
+class Book
+
+attr_accessor :due_date
+
+@@on_shelf = [] # the collection of book objects that are available to be lent out
+
+@@on_loan = [] # the collection of books that are currently being borrowed
+
+
+
+
+
+end#Book
