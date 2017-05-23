309dbced82c49f8d7ee4b7e240d1d7458181ea96
mikesmall <michael.l.a.small@gmail.com>
2017-05-23 15:46:46 -0400

    Corrected syntax for 'class methods'

--- part_1_bank_account.rb
+++ part_1_bank_account.rb
@@ -17,7 +17,7 @@ class BankAccount
   end#initialize
 
   # instance method called deposit that accepts a number as an argument and adds that amount to the account's balance
-  def @deposit(deposit)
+  @deposit(deposit)
   deposit = deposit.to_f
     @balance = balance += deposit
   end#@deposit
@@ -30,21 +30,21 @@ class BankAccount
 
   # STEP 7:
   # Add a class method called create that calls BankAccount.new and adds the new object to @@accounts so that we can find it again in the future. This method should return the new account object. This needs to be a class method because at the time we run it there is no single, specific account object that we are working on.
-  def @@create(new_account)
+  def self.create
     new_account = BankAccount.new
     @@accounts << new_account
     return new_account
-  end#@@create
+  end#self.create
 
   # STEP 8:
   # Add a class method called total_funds that returns the sum of all balances across all accounts in @@accounts.
-  def @@total_funds(sum_of_all_balances)
+  def self.total_funds
     sum_of_all_balances = @@accounts.sum
-  end#@@total_funds
+  end#self.total_funds
 
   # STEP 9:
   # This needs to be a class method because it does not pertain to any single, specific account. Add a class method called interest_time that iterates through all accounts and increases their balances according to @@interest_rate.
-  def @@interest_time(account)
+  def self.interest_time
     @@accounts.each do
       interest_div = @@interest_rate / 100.0  # changes 2.5 to 0.025
       interest_add = interest_div + 1         # changes 0.025 to 1.025
