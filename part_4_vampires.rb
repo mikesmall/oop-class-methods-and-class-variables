c4bf53ca281b102c38c99eb125df1d9bae1854c7
mikesmall <michael.l.a.small@gmail.com>
2017-05-24 00:44:09 -0400

    Part 4 (Vampires) troubleshooting stage

--- part_4_vampires.rb
+++ part_4_vampires.rb
@@ -4,34 +4,48 @@ class Vampire
 
   @@list_of_vampires = []
 
-  def initialize(name, age, in_coffin, drank_blood_today)
+  def initialize(name, age)
     @name = name
     @age = age
-    @in_coffin =
-    @drank_blood_today =
   end#initialize
 
   def self.create
-    new_vampire = Vampire.new(name, age, in_coffin, drank_blood_today)
+    new_vampire = Vampire.new
     @@list_of_vampires << new_vampire
     return new_vampire
   end#@@create
 
   def self.drink_blood
-    @drank_blood_today = true
+    @drank_blood_today == true
   end#drink_blood
 
   def self.go_home
-    @in_coffin = true
+    @in_coffin == true
   end#go_home
 
-  def @@sunrise
+  def sunrise
     # removes from the coven any vampires who are out of their coffins or who haven't drank any blood in the last day
+    @@list_of_vampires.each do |vampire|
+      if vampire.in_coffin == false
+        @@list_of_vampires.delete(self)
+      elsif vampire.drank_blood_today == false
+        @@list_of_vampires.delete(self)
+      end#if
+    end#each
   end#@@sunrise
 
-  def @@sunset
+  def sunset
     # sets drank_blood_today and in_coffin to false for the entire coven as they go out in search of blood
+    @@list_of_vampires.each do
+      drank_blood_today == false
+      in_coffin == false
+    end#each
   end#@@sunset
 
-
 end#Vampire
+
+# Test outputs
+
+mike = Vampire.create("Mike", 38)
+erin = Vampire.create("Erin", 35)
+puts @@list_of_vampires
